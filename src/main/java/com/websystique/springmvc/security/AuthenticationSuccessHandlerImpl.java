535cabe851ca23ab2b1b8fc85f8b8fc1fd5d22f7
Vladimir Filippov <sergeiych@gmail.com>
2019-12-18 02:16:38 +0300

    Упростил AuthenticationSuccessHandler

--- src/main/java/com/websystique/springmvc/security/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/com/websystique/springmvc/security/AuthenticationSuccessHandlerImpl.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
 
-public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
--- src/main/java/com/websystique/springmvc/security/SecurityConfiguration.java
+++ src/main/java/com/websystique/springmvc/security/SecurityConfiguration.java
@@ -61,6 +61,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
-		return new MySimpleUrlAuthenticationSuccessHandler();
+		return new AuthenticationSuccessHandlerImpl();
 	}
 }
