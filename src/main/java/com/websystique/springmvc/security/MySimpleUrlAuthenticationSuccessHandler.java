aacbea74214968dc3760d5b21b4edbfbd47266c3
Vladimir Filippov <sergeiych@gmail.com>
2019-12-17 22:27:45 +0300

    Упростил AuthenticationSuccessHandler

--- src/main/java/com/websystique/springmvc/security/MySimpleUrlAuthenticationSuccessHandler.java
+++ src/main/java/com/websystique/springmvc/security/MySimpleUrlAuthenticationSuccessHandler.java
@@ -1,20 +1,12 @@
 package com.websystique.springmvc.security;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Set;
 
 public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
